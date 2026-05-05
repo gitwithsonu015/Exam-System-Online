1ecc6589baa65e0b557698351954dbdcf8dfaf68
gitwithsonu015 <sonudigital245@gmail.com>
2026-05-05 15:14:32 +0530

    oes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://online-exam-system-14-9bt0.onrender.com/api',
         changeOrigin: true
       }
     }
